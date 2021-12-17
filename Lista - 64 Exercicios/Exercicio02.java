be3be1d4fe243607c5dc9361a1b76fa3a952df6d
Hellimateas Barros <hellimateasbarros@gmail.com>
2021-12-17 18:39:20 -0300

    atividade 2 sobre tipos primitivos

---
+++ Lista - 64 Exercicios/Exercicio02.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+/*
+Faça um programa que: 
+- Leia a cotação do dólar 
+- Leia um valor em dólares 
+- Converta esse valor para Real 
+- Mostre o resultado
+*/
+public class Exercicio02 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Digite a cotação do dólar hoje: ");
+        double cotacaoDolar = input.nextDouble();
+
+        System.out.println("Digite o valor em dólares: ");
+        double dolar = input.nextFloat();
+
+        double real = cotacaoDolar * dolar;
+
+        System.out.printf("O valor em reais é %f", real);
+        input.close();
+    }
+}
