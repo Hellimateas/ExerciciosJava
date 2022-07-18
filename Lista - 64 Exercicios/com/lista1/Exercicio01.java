455a14f9916dcfcdfd7b7dd0500bad89f273a4d3
Hellimateas Chaves <91029460+Hellimateas@users.noreply.github.com>
2022-07-18 20:02:37 -0300

    editei a identação do codigo

--- Lista - 64 Exercicios/com/lista1/Exercicio01.java
+++ Lista - 64 Exercicios/com/lista1/Exercicio01.java
@@ -2,7 +2,7 @@ package com.lista1;
 import java.util.Scanner;
 
 // Faça um programa para calcular o estoque médio de uma peça, sendo que:
-// ESTOQUE MÉDIO = (QUANTIDADE_M�?NIMA + QUANTIDADE_M�?XIMA) / 2.
+// ESTOQUE MÉDIO = (QUANTIDADE_M�?NIMA + QUANTIDADE_M�?XIMA) / 2.
 
 public class Exercicio01 {
     public static void main(String[]args){
@@ -11,12 +11,12 @@ public class Exercicio01 {
         System.out.println("Digite a quantidade máxima do estoque: ");
         int quantidadeMaxima = input.nextInt();
 
-         System.out.println("Digite a quantidade mínima do estoque: ");
-         int quantidadeMinima = input.nextInt();
+        System.out.println("Digite a quantidade mínima do estoque: ");
+        int quantidadeMinima = input.nextInt();
 
-         int estoqueMedia = (quantidadeMaxima+quantidadeMinima)/2;
+        int estoqueMedia = (quantidadeMaxima+quantidadeMinima)/2;
 
-         System.out.printf("A quantidade média do estoque é %d ", estoqueMedia);
+        System.out.printf("A quantidade média do estoque é %d ", estoqueMedia);
         
 
         input.close();
